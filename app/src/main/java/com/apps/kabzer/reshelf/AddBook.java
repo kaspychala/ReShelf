3498bd5ca9b26eac18ef838ec53eb05ef3e00d7d
Kasper <kaspychala95@gmail.com>
2016-05-26 17:55:36 +0200

    Layout fix and strings clean.

--- app/src/main/java/com/apps/kabzer/reshelf/AddBook.java
+++ app/src/main/java/com/apps/kabzer/reshelf/AddBook.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -14,8 +16,8 @@ import android.widget.EditText;
 public class AddBook extends AppCompatActivity{
     EditText author;
     EditText title;
-    Button addBook;
     Intent intent;
+    DatabaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +26,8 @@ public class AddBook extends AppCompatActivity{
 
         author = (EditText)findViewById(R.id.EditAuthor);
         title = (EditText)findViewById(R.id.EditTitle);
-        addBook = (Button)findViewById(R.id.add);
         intent = new Intent(this,ReshelfActivity.class);
-
-        final DatabaseHandler db = new DatabaseHandler(this);
-
-        addBook.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                db.addBook(new Books(author.getText().toString(), title.getText().toString()));
-                startActivity(intent);
-                finish();
-                }
-        });
+        db = new DatabaseHandler(this);
 
         FloatingActionButton addPhoto = (FloatingActionButton) findViewById(R.id.addPhoto);
         addPhoto.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +41,34 @@ public class AddBook extends AppCompatActivity{
 
     @Override
     public void onBackPressed() {
-       finish();
+        startActivity(intent);
+        finish();
+
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_book, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+            db.addBook(new Books(author.getText().toString(), title.getText().toString()));
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
